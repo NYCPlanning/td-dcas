1648621e2f66249032592fee2482fed4580b3c9d
mayijun1203 <mayijun1203@gmail.com>
2020-04-21 18:31:35 -0400

    Update dcas.py

--- dcas.py
+++ dcas.py
@@ -410,8 +410,8 @@ facilityparcelheat.crs={'init':'epsg:4326'}
 dcasct=gpd.read_file(path+'OUTPUT/dcasct.shp')
 dcasct.crs={'init':'epsg:4326'}
 dcasindexparcel=gpd.sjoin(facilityparcelheat,dcasct,how='inner',op='intersects')
-dcasindexparcel['facindex']=pd.qcut(dcasindexparcel['facilitypa'],100,labels=False)+1
-dcasindexparcel['spdindex']=100-pd.qcut(dcasindexparcel['avgspeed'],100,labels=False)
+dcasindexparcel['facindex']=pd.qcut(dcasindexparcel['facilitypa'],50,labels=False)+1
+dcasindexparcel['spdindex']=50-pd.qcut(dcasindexparcel['avgspeed'],50,labels=False)
 dcasindexparcel['dcasindex']=(dcasindexparcel['facindex']+dcasindexparcel['spdindex'])/2
 dcasindexparcel=dcasindexparcel[['facindex','spdindex','dcasindex','geometry']].reset_index(drop=True)
 dcasindexparcel.to_file(path+'OUTPUT/dcasindexparcel.shp')
